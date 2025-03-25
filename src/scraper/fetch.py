d5f385e020fa06bef83ed0995ce00ad7d2d57598
ludic <notetakingforeggs@gmail.com>
2025-03-25 13:19:22 +0000

    feat: removing pandas and re-implementing link scraping

--- src/scraper/fetch.py
+++ src/scraper/fetch.py
@@ -1,35 +1,30 @@
 # from requests import Session
 import requests
 # from scraper.session import BASE_URL
-import pandas as pd
-
+from bs4 import BeautifulSoup as bs
 
 def get_court_links(session) -> list[str] :
     """Retrieves all court links from the main court page."""
     print("getting court links")
     try:
         BASE_URL = "https://www.courtserve.net"
-        url = BASE_URL + "/courtlists/current/county/indexv2county.php"
+        url = BASE_URL + "/courtlists/current/county/indexv2county.php"  
+            
         response = session.get(url)
-        tables = pd.read_html(response.text)
+        soup = bs(response.text, "html.parser")
 
-        first_table_df = tables[0]
-        print(first_table_df.head())   
+        table = soup.find_all("table")[0]
 
-        return first_table_df
+        if not table:
+            print("No tables found.")
+            return []
         
-        
-        
-        # response = session.get(BASE_URL + "/courtlists/current/county/indexv2county.php")
-        # soup = bs(response.text, "html.parser")
-        # box = soup.find(id="box2a")
-        # if not box:
-        #     print("No court links found.")
-        #     return []
-        # links = box.find_all("a", string = lambda text: text and "daily" in text.lower())
-        # for link in links:
-        #     print(link.get("href"))
+        links = table.find_all("a", string = lambda text: text and "daily" in text.lower())
+
+        for link in links:
+            print(link.get("href"))
         # return [link.get("href") for link in box.find_all("a") if link.get("href")]
+        return[]
 
     except requests.RequestException as e:
         print(f"Failed to retrieve court links: {e}")
