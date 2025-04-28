49721ed62545f002baeec0886f1129fb3f18ab5b
ludic <notetakingforeggs@gmail.com>
2025-04-28 12:59:49 +0100

    feat: adding flavours for distinguishing different court types - parser

---
+++ src/scraper/flavours/flavours.py
@@ -0,0 +1,21 @@
+from typing import NamedTuple
+from scraper.parsers.flavour_1_daily_cause_list_parser import Flavour1DailyCauseListParser
+from scraper.parsers.flavour_2_daily_cause_list_parser import Flavour2DailyCauseListParser
+
+from bs4 import BeautifulSoup as bs
+
+class Flavour(NamedTuple):
+    parser_class : type
+    factory_class: type
+
+FLAVOURS: dict[str, Flavour] = {
+    "flavour1": Flavour(Flavour1DailyCauseListParser, Flavour1CourtCaseFactory),
+    "flavour2": Flavour(Flavour2DailyCauseListParser, Flavour2CourtCaseFactory),
+}
+
+def detect_Flavour(html:str) -> Flavour:
+    soup = bs(html, "html.parser")
+    if soup.find("meta", {"name": "Generator"}):
+        return FLAVOURS["flavour1"]
+    else:
+        return FLAVOURS ["flavour2"]
